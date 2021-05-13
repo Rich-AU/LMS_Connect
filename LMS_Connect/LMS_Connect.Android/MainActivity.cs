adece2e2561faa9ff2d4d8fee8faf7b45a0e8b40
Rich-AU <62315253+Rich-AU@users.noreply.github.com>
2021-05-13 22:43:03 +1000

    Add project files.

---
+++ LMS_Connect/LMS_Connect.Android/MainActivity.cs
@@ -0,0 +1,165 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+using System.Net.Http;
+using Xamarin.Essentials;
+
+namespace LMS_Connect.Droid
+{
+    [Activity(Label = "LMS_Connect", Name = "com.companyname.lms_connect.MainActivity", Icon = "@mipmap/play", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+
+            if (Intent.ActionSend.Equals(Intent.Action) && Intent.Type != null && "text/plain".Equals(Intent.Type))
+            {
+                handleSendUrl();
+            }
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        private void handleSendUrl()
+        {
+            if (!Preferences.ContainsKey("protocol") || !Preferences.ContainsKey("LMSServer") || !Preferences.ContainsKey("port") || !Preferences.ContainsKey("c_id"))
+            {
+                FinishAndRemoveTask();
+                FinishAffinity();
+            }
+            var DefinedProtocol = Preferences.Get("protocol", "http");
+            var DefinedLMSServer = Preferences.Get("LMSServer", "");
+            var DefinedPort = Preferences.Get("port", "9000");
+            var DefinedPlayerid = Preferences.Get("c_id", "");
+
+
+
+            var url = Intent.GetStringExtra(Intent.ExtraText);
+            var ServiceProvider = "";
+            StringComparison comp = StringComparison.OrdinalIgnoreCase;
+            if (url.Contains("tidal.com", comp))
+            { ServiceProvider = "Tidal"; } 
+            else if(url.Contains("Qobuz.com", comp)) ServiceProvider = "Qobuz";
+            if (ServiceProvider == "")
+			{
+                FinishAndRemoveTask();
+                FinishAffinity();
+            }
+			else
+			{
+                var DomainIndex = url.IndexOf(".com/")+5;
+                var info = url.Substring(DomainIndex).Split("/");
+                if (info.Length>=2)
+				{
+                    url = "Service Provider:" + ServiceProvider + " Type:" + info[0] + " ID:" + info[1];
+
+
+                    //string playerid = "b8:27:eb:8f:24:4b";
+                    string cmdPara="";
+
+                    if (ServiceProvider == "Tidal" )                       
+					{
+
+                        if (info[0].Equals("track", StringComparison.OrdinalIgnoreCase))
+                        {
+                                cmdPara = "\"playlist\",\"play\",\"wimp://" + info[1] + ".flac\"";
+                            }
+                        else if (info[0].Equals("album", StringComparison.OrdinalIgnoreCase))
+                        {
+                            cmdPara = "\"playlist\",\"play\",\"wimp://album:" + info[1] + ".tdl\"";
+                        }
+                        else if (info[0].Equals("playlist", StringComparison.OrdinalIgnoreCase))
+                        {
+                            cmdPara = "\"playlist\",\"play\",\"wimp://playlist:" + info[1] + ".tdl\"";
+                        }
+
+                    }
+                    else
+					{
+                        if (info[0].Equals("track", StringComparison.OrdinalIgnoreCase))
+                        {
+                            cmdPara = "\"playlist\",\"play\",\"qobuz://" + info[1] + ".flac\"";
+                        }
+                        else if (info[0].Equals("album", StringComparison.OrdinalIgnoreCase))
+                        {
+                            cmdPara = "\"playlist\",\"play\",\"qobuz://album:" + info[1] + ".qbz\"";
+                        }
+                        else if (info[0].Equals("playlist", StringComparison.OrdinalIgnoreCase))
+                        {
+                            cmdPara = "\"playlist\",\"play\",\"qobuz://playlist:" + info[1] + ".qbz\"";
+                        }
+                    }
+                    if (cmdPara !="")SendLMSRequest(DefinedProtocol, DefinedLMSServer, DefinedPort, DefinedPlayerid, cmdPara);
+
+                }
+				else
+				{
+                    url = "Service Provider:" + ServiceProvider + " format error:" + url.Substring(DomainIndex).Split("/");
+
+                }
+
+               
+
+
+                   FinishAndRemoveTask();
+                   FinishAffinity();
+
+                //var urlTextView = new TextView(this) { Gravity = GravityFlags.Center };
+                //urlTextView.Text = url;
+                //view.AddView(urlTextView);
+
+                //var description = new EditText(this) { Gravity = GravityFlags.Top };
+                //view.AddView(description);
+
+                //new AlertDialog.Builder(this)
+                //    .SetTitle("Save a URL Link")
+                //    .SetMessage("Type a description for your link")
+                //    .SetView(view)
+                //    .SetPositiveButton("Add", (dialog, whichButton) =>
+                //    {
+                //        var desc = description.Text;
+                //    //Save off the url and description here
+
+                //    //Remove dialog and navigate back to app or browser that shared the link
+                //    FinishAndRemoveTask();
+                //        FinishAffinity();
+                //    })
+                //    .Show();
+            }
+
+        }
+
+        private async void SendLMSRequest(string Protocol, string LMSServer, string port, string PlayerID, string CmdPara)
+		{
+            HttpClient client = new HttpClient();
+            Uri uri = new Uri(string.Format(Protocol+"://"+ LMSServer+":"+ port + "/jsonrpc.js", string.Empty));
+
+            string LMSRequest = "{\"id\":1,\"method\":\"slim.request\",\"params\":[\""+ PlayerID + "\",["+CmdPara+ "]]}";
+            StringContent content = new StringContent(LMSRequest);
+			try
+			{
+				HttpResponseMessage response = await client.PostAsync(uri, content);
+			}
+            catch (Exception ex)
+            {  }
+        }
+    }
+}
---
+++ LMS_Connect/LMS_Connect/MainPage.xaml.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using Xamarin.Forms;
+using System.Diagnostics;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Linq;
+using Xamarin.Essentials;
+using System.Collections.ObjectModel;
+
+namespace LMS_Connect
+{
+	public partial class MainPage : ContentPage
+	{
+		private ObservableCollection<string> Itemplayers = new ObservableCollection<string>();
+		List<Player> Players = new List<Player>();
+		public MainPage()
+		{
+			InitializeComponent();
+			 txtProtocol.Text = Preferences.Get("protocol", "http");
+			txtLMSServer.Text = Preferences.Get("LMSServer", "");
+			txtPort.Text = Preferences.Get("port", "9000");
+			if (Preferences.ContainsKey("players")) {
+				Players = JsonConvert.DeserializeObject<List<Player>>(Preferences.Get("players", ""));
+				lstPlayers.Header = "Players: please tap to select the one for streaming.";
+				foreach (Player player in Players)
+				{
+					Itemplayers.Add(player.name);
+				}
+				lstPlayers.IsVisible = true;
+			}
+			this.BindingContext = Itemplayers;
+			if (Preferences.ContainsKey("c_player"))
+			{
+				lstPlayers.Header = "Players: please tap to select the one for streaming. (Current Player:" + Preferences.Get("c_player", "") + ")";
+			}
+
+		}
+		void OnItemTapped(object sender, ItemTappedEventArgs e)
+		{
+			if (e == null) return; // has been set to null, do not 'process' tapped event
+			Preferences.Set("c_player", e.Item.ToString());
+			Player currentPlayer = Players.Find(x => x.name == e.Item.ToString());
+			Preferences.Set("c_id", currentPlayer.playerid);
+			Debug.WriteLine(currentPlayer.playerid);
+			((ListView)sender).SelectedItem = null; // de-select the row
+			lstPlayers.Header = "Players: please tap to select the one for streaming. (Current Player:"+ e.Item+")";
+		}
+
+
+
+		private async void Button_Clicked_1(object sender, EventArgs e)
+		{
+			lblMsg.Text = "Message:";
+			if (txtProtocol.Text.Trim().Length==0 || txtLMSServer.Text.Trim().Length == 0 || txtPort.Text.Trim().Length == 0) 
+			{ lblMsg.Text = "Error:please fill in all LMS server info";lblMsg.TextColor = Color.Red; }
+			else
+			{
+				Preferences.Set("protocol", txtProtocol.Text.Trim());
+				Preferences.Set("LMSServer", txtLMSServer.Text.Trim());
+				Preferences.Set("port", txtPort.Text.Trim());
+
+				HttpClient client = new HttpClient();
+				Uri uri = new Uri(string.Format(txtProtocol.Text.Trim()+"://"+ txtLMSServer.Text.Trim()+":"+ txtPort.Text.Trim()+"/jsonrpc.js", string.Empty));
+
+				string json = "{\"id\":1,\"method\":\"slim.request\",\"params\":[\"\",[\"players\",\"0\",\"5\"]]}";
+				StringContent content = new StringContent(json);
+
+				HttpResponseMessage response = null;
+				try
+				{
+					response = await client.PostAsync(uri, content);
+					var contents = await response.Content.ReadAsStringAsync();
+					Debug.WriteLine(response);
+					JObject ObjPlayers = JObject.Parse(contents);
+					dynamic res = Newtonsoft.Json.JsonConvert.DeserializeObject(contents);
+
+					// get JSON result objects into a list
+					IList<JToken> results = ObjPlayers["result"]["players_loop"].Children().ToList();
+
+					// serialize JSON results into .NET objects
+					if (results.Count > 0)
+					{
+						Players.Clear();
+						lstPlayers.Header = "Players: please tap to select the one for streaming.";
+						Itemplayers.Clear();
+						foreach (JToken result in results)
+						{
+							// JToken.ToObject is a helper method that uses JsonSerializer internally
+							Player player = result.ToObject<Player>();
+							Players.Add(player);
+							Itemplayers.Add(player.name);
+						}
+
+						lstPlayers.IsVisible = true;
+						Preferences.Set("players", JsonConvert.SerializeObject(Players));
+					}
+					else
+					{
+						lblMsg.Text = "Error: No player found"; 
+						lblMsg.TextColor = Color.Red;
+					}
+
+
+				}
+				catch (Exception ex)
+				{
+					lblMsg.Text = "Exception:" +ex.Message;
+					lblMsg.TextColor = Color.Red;
+				}
+			}
+		}
+			
+	}
+
+	public class Player
+	{
+		public string name { get; set; }
+		public string playerid { get; set; }
+	}
+
+}
